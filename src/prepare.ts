842cf3f2fddd7ee853d587643afaa00b52e05f84
Martin Algesten <martin@algesten.se>
2019-01-11 17:11:25 +0530

    filter undefined values

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,3 +1,5 @@
+// tslint:disable no-object-mutation
+
 import { isWellKnown } from ".";
 
 /**
@@ -38,23 +40,24 @@ export const prepareLog = (
     }
     const message = String(args[0]);
     const wellRaw = args.length >= 2 ? args[1] : {};
-    const data = args.length == 3 ? args[2] : {};
+    const dataRaw = args.length == 3 ? args[2] : {};
+
+    // our own local copies, so we can delete without affecting the input
+    const well = filterUndefined(wellRaw);
+    const data = filterUndefined(dataRaw);
 
     // ensure well know really only contains well known fields
-    if (!isWellKnown(wellRaw, (msg) => console.log('Ignoring log row:', msg))) {
+    if (!isWellKnown(well, (msg) => console.log('Ignoring log row:', msg))) {
         return;
     }
 
-    // our own local copy, so we can delete without affecting the input
-    const well = { ...wellRaw };
-
     const timestamp = well.timestamp || Date.now();
-    // tslint:disable-next-line:no-object-mutation
     delete well.timestamp;
 
     const dataLen = Object.keys(data).length;
-    const mergeLen = Object.keys(well).length + dataLen;
-    const merged = mergeLen > 0 ? { ...well, ...(dataLen ? { data } : {}) } : undefined;
+    const mergaroo: { [key: string]: any } = { ...well, ...(dataLen ? { data } : {}) };
+
+    const merged = Object.keys(mergaroo).length ? mergaroo : undefined;
 
     return {
         severity,
@@ -65,3 +68,14 @@ export const prepareLog = (
     };
 };
 
+// delete any undefined/null etc values
+const filterUndefined = (oin: any): any => {
+    const o = { ...oin };
+    for (const k of Object.keys(o)) {
+        const v = o[k];
+        if (v == null) { // deliberate ==
+            delete o[k];
+        }
+    }
+    return o;
+};
--- test/test-well-known.ts
+++ test/test-well-known.ts
@@ -18,3 +18,12 @@ test('log recordingId and userId', async t => {
     t.is(m, `<134>1 2019-01-10T07:22:49.669Z testhost test-app ${pid}` +
         ` - [apikey@41058 env="testing"] hi {"recordingId":"r123","userId":"u123"}\n`);
 });
+
+test('log recordingId as undefined', async t => {
+    const { msg, log } = await createMockLogger();
+    log.info('hi', { timestamp: 1547104969669, recordingId: undefined });
+    const m = await msg;
+    const pid = process.pid;
+    t.is(m, `<134>1 2019-01-10T07:22:49.669Z testhost test-app ${pid}` +
+        ` - [apikey@41058 env="testing"] hi\n`);
+});
