19a6645f21b20a9e09c792bfcd9e5b1d8eb65691
Martin Algesten <martin@algesten.se>
2019-01-23 12:00:02 +0530

    enable loggly

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createSyslogger, LoggerImpl } from "./syslog";
 import { createConsLogger } from "./conslog";
 import { Severity, prepareLog } from "./prepare";
-// import * as isBrowser from "is-browser";
-// import { createLogglyLogger } from "./loggly";
+import * as isBrowser from "is-browser";
+import { createLogglyLogger } from "./loggly";
 import { mkValidator } from "./validator";
 
 /**
@@ -285,8 +285,8 @@ export const createLogger = (opts: Options): Logger => {
 
     // create the syslog instance
     // TODO remove the loggly logger here when we are rolled out with our own ingester.
-    const syslogger = createSyslogger(opts);
-    // const syslogger = isBrowser ? createLogglyLogger(opts) : createSyslogger(opts);
+    // const syslogger = createSyslogger(opts);
+    const syslogger = isBrowser ? createLogglyLogger(opts) : createSyslogger(opts);
 
     // for testing we can rig the output
     const output = (opts as any).__output || console;
