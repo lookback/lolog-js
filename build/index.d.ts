8017ebb6f96c3ae3210d480d757269bd50c47427
Martin Algesten <martin@algesten.se>
2019-01-30 20:16:05 +0530

    build

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,4 @@
+export { serializeError } from './prepare';
 /**
  * The overloaded variants of logging.
  */
--- build/prepare.d.ts
+++ build/prepare.d.ts
@@ -24,5 +24,12 @@ export interface PreparedLog {
  * Helper to prepare a log message, rejecting it if it's not well formed.
  */
 export declare const prepareLog: (severity: Severity, appName: string, args: any[]) => PreparedLog | undefined;
+export interface SerializedError {
+    name: string;
+    message: string;
+    stack?: string;
+}
+/** Serialize and Error to a plain object, keeping commonly used properties. */
+export declare const serializeError: (err: Error) => SerializedError;
 /** Recursive helper to remove complex objects. */
 export declare const filterUnwanted: (oin: any) => any;
