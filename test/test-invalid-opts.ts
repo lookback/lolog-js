85015c2c5d30d2629bc0f377ec8d1592799abf55
Martin Algesten <martin@lookback.io>
2020-01-30 19:05:03 +0100

    fix test

--- test/test-invalid-opts.ts
+++ test/test-invalid-opts.ts
@@ -3,7 +3,11 @@ import { createLogger, Options } from '../src/index';
 import assert from 'assert';
 
 test('create with missing logHost', () => {
-    assert.throws(() => createLogger({
-        // .. uh oh
-    } as Options), 'Invalid options: logHost is a required field');
+    assert.throws(
+        () =>
+            createLogger({
+                // .. uh oh
+            } as Options),
+        'create with missing logHost'
+    );
 });
