20cb82a61ed4ab4d963d1986fffdc4c4c6832fdd
Martin Algesten <martin@lookback.io>
2020-06-04 15:17:50 +0200

    build

---
+++ build/cjs/is-browser.js
@@ -0,0 +1,5 @@
+"use strict";
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isBrowser = !!((_b = (_a = window) === null || _a === void 0 ? void 0 : _a.navigator) === null || _b === void 0 ? void 0 : _b.mediaDevices);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUdhLFFBQUEsU0FBUyxHQUFHLENBQUMsY0FBQyxNQUFNLDBDQUFFLFNBQVMsMENBQUUsWUFBWSxDQUFBLENBQUMifQ==
