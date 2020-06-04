1774c3b9c4a078981759c00acd6c3c1437441a23
Martin Algesten <martin@lookback.io>
2020-06-04 16:15:10 +0200

    build

--- build/cjs/is-browser.js
+++ build/cjs/is-browser.js
@@ -1,7 +1,5 @@
 "use strict";
-var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
-// tslint:disable-next-line: no-var-keyword
-var window;
-exports.isBrowser = !!((_b = (_a = window) === null || _a === void 0 ? void 0 : _a.navigator) === null || _b === void 0 ? void 0 : _b.mediaDevices);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLDJDQUEyQztBQUMzQyxJQUFJLE1BQVcsQ0FBQztBQUNILFFBQUEsU0FBUyxHQUFHLENBQUMsY0FBQyxNQUFNLDBDQUFFLFNBQVMsMENBQUUsWUFBWSxDQUFBLENBQUMifQ==
+const checkBrowser = new Function('try {return this===window;}catch(e){ return false;}');
+exports.isBrowser = checkBrowser();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQ0EsTUFBTSxZQUFZLEdBQUcsSUFBSSxRQUFRLENBQzdCLHFEQUFxRCxDQUN4RCxDQUFDO0FBRVcsUUFBQSxTQUFTLEdBQVksWUFBWSxFQUFFLENBQUMifQ==
