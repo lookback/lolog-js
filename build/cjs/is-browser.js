81ea63d908a82b19b2588336811704f0abc1e1fd
Martin Algesten <martin@lookback.io>
2020-06-04 15:24:30 +0200

    build

--- build/cjs/is-browser.js
+++ build/cjs/is-browser.js
@@ -1,5 +1,7 @@
 "use strict";
 var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
+// tslint:disable-next-line: no-var-keyword
+var window;
 exports.isBrowser = !!((_b = (_a = window) === null || _a === void 0 ? void 0 : _a.navigator) === null || _b === void 0 ? void 0 : _b.mediaDevices);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUdhLFFBQUEsU0FBUyxHQUFHLENBQUMsY0FBQyxNQUFNLDBDQUFFLFNBQVMsMENBQUUsWUFBWSxDQUFBLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLDJDQUEyQztBQUMzQyxJQUFJLE1BQVcsQ0FBQztBQUNILFFBQUEsU0FBUyxHQUFHLENBQUMsY0FBQyxNQUFNLDBDQUFFLFNBQVMsMENBQUUsWUFBWSxDQUFBLENBQUMifQ==
--- build/es/is-browser.js
+++ build/es/is-browser.js
@@ -1,3 +1,5 @@
 var _a, _b;
+// tslint:disable-next-line: no-var-keyword
+var window;
 export const isBrowser = !!((_b = (_a = window) === null || _a === void 0 ? void 0 : _a.navigator) === null || _b === void 0 ? void 0 : _b.mediaDevices);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFHQSxNQUFNLENBQUMsTUFBTSxTQUFTLEdBQUcsQ0FBQyxjQUFDLE1BQU0sMENBQUUsU0FBUywwQ0FBRSxZQUFZLENBQUEsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXMtYnJvd3Nlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9pcy1icm93c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFDQSwyQ0FBMkM7QUFDM0MsSUFBSSxNQUFXLENBQUM7QUFDaEIsTUFBTSxDQUFDLE1BQU0sU0FBUyxHQUFHLENBQUMsY0FBQyxNQUFNLDBDQUFFLFNBQVMsMENBQUUsWUFBWSxDQUFBLENBQUMifQ==
