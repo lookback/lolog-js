6629318bedaa190a8bcaa87fb816ef5aeb90f7ff
Martin Algesten <martin@lookback.io>
2020-08-05 16:59:18 +0200

    fix broken test for setDebug going

--- test/test-severity.ts
+++ test/test-severity.ts
@@ -9,26 +9,9 @@ test('not send trace', async () => {
     assert.deepEqual(m, 'not sent');
 });
 
-test('by default not send debug', async () => {
+test('default will send debug', async () => {
     const { msg, log } = await createMockLogger();
     log.debug('hello world', <any>{ timestamp: 1547104969669 });
     const m = await msg.catch(() => 'not sent');
-    assert.deepEqual(m, 'not sent');
-});
-
-test('send debug if enabled', async () => {
-    const { msg, log } = await createMockLogger();
-    log.setDebug(true);
-    log.debug('hello world', <any>{ timestamp: 1547104969669 });
-    const m = await msg;
-    assert.deepEqual(m, `<135>1 2019-01-10T07:22:49.669Z testhost test-app 2.11` +
-        ` - [u@53595 apiKey="apikey" env="testing"] hello world\n`);
-});
-
-test('not send trace even if debug is enabled', async () => {
-    const { msg, log } = await createMockLogger();
-    log.setDebug(true);
-    log.trace('hello world', <any>{ timestamp: 1547104969669 });
-    const m = await msg.catch(() => 'not sent');
-    assert.deepEqual(m, 'not sent');
+    assert.deepEqual(m, '<135>1 2019-01-10T07:22:49.669Z testhost test-app 2.11 - [u@53595 apiKey="apikey" env="testing"] hello world\n');
 });
